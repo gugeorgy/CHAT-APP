699a89d483f154fa1e84fe1e151648b967ca1cc2
gugeorgy <gueigeorgy2@gmail.com>
2024-03-30 19:38:31 +0100

    :tada: initialize full-stack chat-app with client and server modules

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config({ path: '.env.development.local' });
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
