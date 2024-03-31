ccd1fb1fe331ffa5127cbfdd4ff7fda0c80aa7cf
gugeorgy <gueigeorgy2@gmail.com>
2024-03-31 18:45:39 +0200

    :bust_in_silhouette: feat(signIn): implement form with react-hook-form for signIn component

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import SignIn from '@/components/signin';
+import { Box, Heading } from '@/components/ui';
+
+export default function Login() {
+  return (
+    <Box>
+      <Heading>Login Chat App</Heading>
+      <SignIn />
+    </Box>
+  );
+}
--- client/src/components/ui/index.ts
+++ client/src/components/ui/index.ts
@@ -2,8 +2,6 @@
 
 import { Image, type ImageProps, Link, type LinkProps } from '@chakra-ui/next-js';
 
-import FadeText from './fade-text';
-
-export { Link, Image, FadeText };
+export { Link, Image };
 export type { LinkProps, ImageProps };
 export * from '@chakra-ui/react';
